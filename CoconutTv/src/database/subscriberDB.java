c0376903193dd19b322fc00b01caee4c7859a3dc
Jose Rodriguez <jrodr3502@365.elmhurst.edu>
2018-11-07 11:18:52 -0600

    I started the database interface. The most logical way to organize this is to have a database interface class for each table / group. By this i mean that the movie table will have its on DB interface class with prepared statements to manipulate the table. In this file is the begining of the subscriber interface class. It shows how we establish a connection and then proceed to create update scripts that will be sent to the database when we would like to update a subscriber or add a new one. The delete and other statements need to be prepared as well but this is a good place to start to get our heads wrapped around this. Also the name of all the getters are subject based on the naming convention that Shayne implemeted in his construction of the classes.

---
+++ CoconutTv/src/database/subscriberDB.java
@@ -0,0 +1,112 @@
+package database;
+
+import java.sql.*;
+import java.util.*;
+
+//import the package.(name of the class that is being used)
+
+public class subscriberDB {
+
+	private Connection con = null;
+	private PreparedStatement dbQuery;
+	
+	
+	public void initializaJdbc() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriveManager.getconnection
+				("jdbc:mysql://localhost/moviestoredb" , "scott", "tiger");
+	}
+	catch (Exceptioni ex) {
+		ex.printStackTrace();
+		}
+	}
+
+	public Connection getConnection() {
+		return con;
+	}
+	
+	public void updateSubscriber(Subscriber changedSub) throws SQLException {
+		String personalInfo = "update subscriber set levelName = ?, firstName = ?, lastName = ?, phoneNumber = ?, emailAddress = ?, memberPassword = ?, accountStatus = ? where accountID = ?";
+		String addressInfo = "update subscriber set billAddressLine1 = ?, billAddressLine2 = ?, billCity = ?, billState = ?, billZipCode = ? where accountID = ?";
+		String cardInfo = "update card set creditCardCCV = ?, creditCardNumber = ?, cardHolderFristName = ?, cardHolderLastName = ?, expYear = ?, expMonth = ?, ccType = ? where accountID = ?";
+		
+		dbQuery = con.preparedStatement(personalInfo);
+		
+		dbQuery.setString(1, changedSub.getlevelName());
+		dbQuery.setString(2, changedSub.getFirstName());
+		dbQuery.setString(3, changedSub.getLastName());
+		dbQuery.setString(4, changedSub.getPhoneNumber());
+		dbQuery.setString(5, changedSub.getEmailAddress());
+		dbQuery.setString(6, changedSub.getMemberPassword());
+		dbQuery.setString(7, changedSub.getAccountStarus());
+		dbQuery.setInt(8, changedSub.getAccountID());
+		dbQuery.executeUpdate();
+		
+		dbQuery = con.preparedStatement(addressInfo);
+		
+		dbQuery.setString(1, changedSub.getBillAddressLine1());
+		dbQuery.setString(2, changedSub.getBillAddressLine2());
+		dbQuery.setString(3, changedSub.getBillCity());
+		dbQuery.setString(4, changedSub.getBillState());
+		dbQuery.setString(5, changedSub.getBillZipCode());
+		dbQuery.setInt(6, changedSub.getAccountID());
+		dbQuery.executeUpdate();
+		
+		dbQuery = con.preparedStatement(cardInfo);
+		
+		dbQuery.setInt(1, changedSub.getCreditCardCCV());
+		dbQuery.setString(2, changedSub.getCreditCardNumber());
+		dbQuery.setString(3, changedSub.getFirstName());
+		dbQuery.setString(4, changedSub.getLastName();
+		dbQuery.setInt(5, changedSub.getExpYear());
+		dbQuery.setInt(6, changedSub.getExpMonth());
+		dbQuery.setString(7, changedSub.getCCType());
+		dbQuery.setInt(8, changedSub.getAccountID());
+		dbQuery.executeUpdate();
+		
+	}
+	
+	public void addSubscriber(Subscriber addedSub) throws SQLException {
+		String subInfo = "Insert into subscriber (levelName, firstName, lastName, billAddressLine1, billAddressLine2, billCity, billState, billZipCode, phoneNumber, emailAddress, memberPassword, accountCreateDate, accountStatus) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String cardInfo = "Insert into card (accountID, creditCardCCV, creditcardNumber, cardholderfirstname, cardholderlastname, expyear,expmonth, cctype) values (?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		dbQuery = con.preparedStatement(subInfo);
+		
+		dbQuery.setString(1, addedSub.getlevelName());
+		dbQuery.setString(2, addedSub.getFirstName());
+		dbQuery.setString(3, addedSub.getLastName());
+		dbQuery.setString(4, addedSub.getBillAddressLine1());
+		dbQuery.setString(5, addedSub.getBillAddressLine2());
+		dbQuery.setString(6, addedSub.getBillCity());
+		dbQuery.setString(7, addedSub.getBillState());
+		dbQuery.setString(8, addedSub.getBillZipCode());
+		dbQuery.setString(9, addedSub.getPhoneNumber());
+		dbQuery.setString(10, addedSub.getEmailAddress());
+		dbQuery.setString(11, addedSub.getMemberPassword());
+		dbQuery.setDate(12, new java.util.Date());
+		dbQuery.setString(13, addedSub.getAccountStatus());
+		dbQuery.executeUpdate();
+		
+		int maxID = 0;
+		dbQuery = con.preparedStatement("select MAX(accointID) from subscriber");
+		ResultSet rset = dbQuery.executeQuery();
+		rset.next();
+		addedSub.setAccountID(rset.getInt(1));
+		
+		dbQuery = con.preparedStatement(cardInfo);
+		
+		dbQuery.setInt(1, addedSub.getAccountID());
+		dbQuery.setInt(2, addedSub.getCreditCardCCV());
+		dbQuery.setString(3, addedSub.getCreditCardNumber());
+		dbQuery.setString(4, addedSub.getFirstName());
+		dbQuery.setString(5, addedSub.getLastName());
+		dbQuery.setInt(6, addedSub.getExpYear());
+		dbQuery.setInt(7, addedSub.getExpMonth());
+		dbQuery.setString(8, addedSub.getCCType());
+		dbQuery.executeUpdate();	
+		
+	}
+}
+
